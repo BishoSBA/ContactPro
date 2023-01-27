3e6049ef19f729eae8c630cfe9da9deb8fa9cfeb
Beshir Saeed <Beshir-saeed@live.com>
2023-01-27 19:20:18 +0300

    Add Models and Home Page

--- ContactPro/Models/Contact.cs
+++ ContactPro/Models/Contact.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+using ContactPro.Enums;
 
 namespace ContactPro.Models
 {
@@ -23,14 +25,38 @@ namespace ContactPro.Models
         [DataType(DataType.Date)]
         public string? BirthDate { get; set; }
         [Required]
-        public string? Address1 { get; set; }
-
-        public string? Address2 { get; set; }
+        public Countries Country { get; set; }
 
         [Required]
         public string? City { get; set; }
-        [Required]
         public string? State { get; set; }
+        [Required]
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        [Display(Name = "Postal Code")]
+        public int PostalCode { get; set; }
 
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; 
+        }
+        
+        //Image properties
+        public byte[]? ImageData { get; set; }
+        public string? ImageType { get; set; }
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; 
+        }
+
+        //virtuals
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
 }
