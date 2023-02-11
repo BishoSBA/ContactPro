f589e4f0c03a750d0aa69e24496b9c0fabc58323
Beshir Saeed <Beshir-saeed@live.com>
2023-02-11 12:22:09 +0300

    Update contacts index page

--- ContactPro/Models/Contact.cs
+++ ContactPro/Models/Contact.cs
@@ -8,22 +8,27 @@ namespace ContactPro.Models
     public class Contact
     {
         public int Id { get; set; }
+
         [Required]
         public string? AppUserId { get; set; }
+
         [Required]
         [Display(Name = "First Name")]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1} characters", MinimumLength = 2)]
         public string? FirstName { get; set; }
+
         [Required]
         [Display(Name = "Last Name")]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1} characters", MinimumLength = 2)]
         public string? LastName { get; set; }
+
         [NotMapped]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
 
         [Display(Name = "Birthday")]
         [DataType(DataType.Date)]
         public DateTime? BirthDate { get; set; }
+
         [Required]
         public Countries Country { get; set; }
 
@@ -33,6 +38,7 @@ namespace ContactPro.Models
         [Required]
         public string? Address1 { get; set; }
         public string? Address2 { get; set; }
+
         [Display(Name = "Postal Code")]
         public int PostalCode { get; set; }
 
